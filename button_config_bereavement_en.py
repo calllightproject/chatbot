333af240685c89b6b567b80352e6875f4d059b33
calllightproject <call.light.project@gmail.com>
2025-12-04 09:28:04 -0800

    Update button_config_bereavement_en.py
    custom note bereave

--- button_config_bereavement_en.py
+++ button_config_bereavement_en.py
@@ -2,7 +2,7 @@
 button_data = {
     # --- Core Notifications & Text ---
     "greeting": "Hello! We are here to support you.",
-    "custom_note_placeholder": "Type your note to the nurse here...",
+    "custom_note_placeholder": "If your request isn’t listed or you need more than one item, please type a short message in the box below.",
     "send_note_button": "Send Note",
     "cna_notification": "✅ CNA has been notified.",
     "nurse_notification": "✅ Nurse has been notified.",
@@ -117,3 +117,4 @@ button_data = {
     "When will I get my discharge paperwork?": {"note": "Once the OB-GYN has put in their notes and discharge orders, your nurse can print out paperwork."},
     "Do I have to take a wheelchair?": {"note": "No, but a staff member has to go with you. If the nursing staff is busy, a transport worker will be the one to escort you out."}
 }
+
